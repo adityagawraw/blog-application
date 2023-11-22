b9a16222830de9c4d6e28a075c97c28ebc4adf7c
aditya <adityagawraw006@gmail.com>
2023-11-22 09:31:48 +0530

    blog page with css added

--- src/main/java/com/aditya/BlogPost/controller/PostController.java
+++ src/main/java/com/aditya/BlogPost/controller/PostController.java
@@ -1,4 +1,14 @@
 package com.aditya.BlogPost.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PostController {
+
+@RequestMapping("/")
+public String getHomePage(){
+    return "home";
+}
+
 }
--- src/main/java/com/aditya/BlogPost/dao/PostDaoImplementation.java
+++ src/main/java/com/aditya/BlogPost/dao/PostDaoImplementation.java
@@ -3,6 +3,7 @@ package com.aditya.BlogPost.dao;
 import com.aditya.BlogPost.entity.Post;
 
 public class PostDaoImplementation implements  PostDao{
+
     @Override
     public void save(Post post) {
 
