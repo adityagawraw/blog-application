8b84bd64d880c609b099c14084b30fbbc9fb90f5
aditya <adityagawraw006@gmail.com>
2023-11-30 07:18:33 +0530

    new data source added

--- src/main/java/com/aditya/BlogPost/restController/PostRestController.java
+++ src/main/java/com/aditya/BlogPost/restController/PostRestController.java
@@ -1,21 +1,33 @@
 package com.aditya.BlogPost.restController;
 
+import com.aditya.BlogPost.dao.PostDao;
+import com.aditya.BlogPost.entity.Post;
 import com.aditya.BlogPost.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class PostRestController {
     private PostService postService;
+    private PostDao postDao;
 
-    public PostRestController(PostService postService) {
+    @Autowired
+    public PostRestController(PostService postService, PostDao postDao) {
         this.postService = postService;
+        this.postDao = postDao;
     }
 
     @GetMapping("/hello")
-    public String getHello(){
-        return "Hello world";
+    public List<Post> getAllPosts(){
+        List<Post> posts = new ArrayList<>();
+        posts = postDao.findAllPosts("ASC");
+        System.out.println(posts);
+        return posts;
     }
 }
